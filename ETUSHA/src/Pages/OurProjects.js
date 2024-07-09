fff714999b704214af64cd66d5db60019a2bbfb1
coderbekhub <coderbek@mail.ru>
2024-07-09 15:15:15 +0500

    ...

--- ETUSHA/src/Pages/OurProjects.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ourProjects() {
-  return (
-    <div>ourProjects</div>
-  )
-}
--- ETUSHA/src/tools/constants.js
+++
@@ -1,5 +0,0 @@
-export const LANGUAGE = "app-manbalar-app"
-
-export const API_PATH = `https://backend.etusha.uz/${
-    localStorage.getItem(LANGUAGE) ? localStorage.getItem(LANGUAGE) : "ky"
-  }/`;
